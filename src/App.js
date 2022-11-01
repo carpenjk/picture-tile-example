670d878a0a990db5f28a3e4fe15c7de0483e0c84
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-11-01 13:18:40 -0400

    test for codesandbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PopupModal from '../base/PopupModal';
+import { useRef } from 'react';
+
 
 function App() {
+  const scrollRef = useRef();
   return (
     <div className="App">
-      <header className="App-header">
+      <header ref={scrollRef} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,6 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <PopupModal isOpen={true} scrollNode={scrollRef} lockScroll />
           Learn React
         </a>
       </header>
